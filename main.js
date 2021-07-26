877c31d34ce06a1594d291b936fc7a7d5b30f5a9
[HippieTaoist] <[shawn.m.kittel@gmail.com]>
2021-07-26 10:44:38 -0600

    tick tock clock initialized

--- main.js
+++ main.js
@@ -1,21 +1,29 @@
 // Setup 'tick' sound
 const tick = new Audio('sounds/tick.mp3');
-const tock = new Audio('sounds/tock.mp3')
+const tock = new Audio('sounds/tock.mp3');
+
+// Internal counter
+counterArea = document.getElementById('counter-area');
 
 //internal tick clock
 let tickNum = 0;
+
+let counterNum = 1
 
 // This function is called every 600ms
 function update() {
     if (tickNum < 4){
         tick.play();
         tickNum += 1
+        counterNum += 1
     }
-    // Play the 'tick' sound
+    // Play the 'tock' sound
     if (tickNum===4){
-        tock.play()
+        tock.play();
         tickNum =0
+        counterNum =1
     }
+    counterArea.innerText =counterNum
 
 
 }
