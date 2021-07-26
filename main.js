3c569a2ca2894e4a2cac06809ef1e9fb425d0bc6
[HippieTaoist] <[shawn.m.kittel@gmail.com]>
2021-07-26 10:01:11 -0600

    audio Tock played on 4th beat

--- main.js
+++ main.js
@@ -1,11 +1,23 @@
 // Setup 'tick' sound
 const tick = new Audio('sounds/tick.mp3');
+const tock = new Audio('sounds/tock.mp3')
+
+//internal tick clock
+let tickNum = 0;
 
 // This function is called every 600ms
 function update() {
-
+    if (tickNum < 4){
+        tick.play();
+        tickNum += 1
+    }
     // Play the 'tick' sound
-    tick.play();
+    if (tickNum===4){
+        tock.play()
+        tickNum =0
+    }
+
+
 }
 
 // This function sets up update() to be called every 600ms
