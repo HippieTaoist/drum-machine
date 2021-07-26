b8f3da0f815d282b5f54e2fd744de881e2b6f994
[HippieTaoist] <[shawn.m.kittel@gmail.com]>
2021-07-26 15:09:12 -0600

    timing corrected on metronome

--- main.js
+++ main.js
@@ -1,29 +1,71 @@
-// Setup 'tick' sound
+// Setup sound
 const tick = new Audio('sounds/tick.mp3');
 const tock = new Audio('sounds/tock.mp3');
+const hihat = new Audio('sounds/hi-hat.mp3')
+const snare = new Audio('sounds/snare-drum.mp3')
+const kick = new Audio('sounds/kick-drum.mp3')
 
 // Internal counter
-counterArea = document.getElementById('counter-area');
+let counterArea = document.getElementById('counter-area');
 
 //internal tick clock
 let tickNum = 0;
-
 let counterNum = 1
 
+// +++++FUNCTIONS++++++++++++++++
 // This function is called every 600ms
 function update() {
-    if (tickNum < 4){
-        tick.play();
-        tickNum += 1
-        counterNum += 1
+    if (tickNum === 4) {
+        tickNum = 0
+        counterNum = tickNum + 1
     }
-    // Play the 'tock' sound
-    if (tickNum===4){
-        tock.play();
-        tickNum =0
-        counterNum =1
+    tickNum += 1
+    playMetronone()
+    playSnareDrum()
+
+
+
+    console.log(tickNum)
+
+
+
+
+    counterArea.innerText = tickNum
+
+
+
+}
+
+function playMetronone() {
+    let playMe = document.getElementById('metronome-checkbox')
+    if (playMe.checked) {
+        if (tickNum < 4) {
+            tick.play()
+        }
+        // Play the 'tock' sound
+        if (tickNum === 4) {
+            tock.play();
+        }
     }
-    counterArea.innerText =counterNum
+}
+
+function playSnareDrum() {
+    let playMe = document.getElementById('snaredrum-checkbox')
+    if (playMe.checked) {
+        snare.play();
+    }
+}
+
+// soundSelector function decides which items are check in the options section and passes them to the beat/metronome area.
+
+// gather boxes into something
+let playSound = document.querySelectorAll('input[name=soundPlay]')
+console.log(playSound)
+
+let snareSound = snare.play()
+
+function soundSelector() {
+
 
 
 }
@@ -33,5 +75,7 @@ function setupUpdate() {
     setInterval(update, 600);
 }
 
+
+// +++++App Calling +++++
 // Call setupUpdate() once after 300ms
 setTimeout(setupUpdate, 300);
